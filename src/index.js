4072dba37c655746deb82ff6c8cdf4132da0e1c1
anibalalpizar <anibal.alpizar14@gmail.com>
2024-10-03 20:15:05 -0600

    Add initial project files and configuration

---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = require("express");
+var app = (0, express_1.default)();
+var PORT = 3000;
+app.listen(PORT, function () { return console.log("Server is running on port ".concat(PORT)); });
