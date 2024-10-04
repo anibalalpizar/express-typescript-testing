e17be434a8504185b67f505d79bdf77d81b22970
anibalalpizar <anibal.alpizar14@gmail.com>
2024-10-03 22:16:33 -0600

    Add user handlers for getting users, getting user by ID, and creating a user

---
+++ src/handlers/users.ts
@@ -0,0 +1,29 @@
+import type { Request, Response } from "express-serve-static-core";
+import type { CreateUserDto } from "../dtos/CreateUser.dto";
+import type { CreateUserQueryParams } from "../types/query-params";
+import type { User } from "../types/response";
+
+export function getUsers(request: Request, response: Response) {
+  response.send([]);
+}
+
+export function getUserById(request: Request, response: Response) {
+  response.send({});
+}
+
+/*
+{1} (e.g., /users/1) -> URL parameters
+{2} request body (what's sent in the response),
+{3} response body (what's in the request body),
+{4} query parameters (e.g., /users?limit=10)
+*/
+export function createUser(
+  request: Request<{}, {}, CreateUserDto, CreateUserQueryParams>,
+  response: Response<User>
+) {
+  return response.status(201).send({
+    id: 1,
+    username: "john_doe",
+    email: "john@gmail.com",
+  });
+}
